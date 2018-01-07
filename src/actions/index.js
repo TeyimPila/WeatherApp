ea681c54b987e4242096d771fc80d2d90ed52183
Teyim Pila <teyim.pila@aun.edu.ng>
2018-01-07 18:25:32 +0100

    successfully retrieve weather for a searched city

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const API_KEY = "5ec090e12404bc4a962dc2176af41883";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city}`;
+    const request = axios.get(url);
+
+    console.log(request);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    }
+}
